931daa34d767b527db5b69da22ac7e669f5f58b8
robin-hassan <robiul.hassan12102@gmail.com>
2023-09-30 11:50:18 -0400

    setup login system with rename app

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,7 +60,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "CsvApp_production"
+  # config.active_job.queue_name_prefix = "data_grid_production"
 
   config.action_mailer.perform_caching = false
 
