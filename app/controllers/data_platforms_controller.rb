df01ddafdb6b197ea37bdd3f8a69e9d7b8162aa3
robin-hassan <robiul.hassan12102@gmail.com>
2023-10-15 00:17:18 -0400

    setup destroy

--- app/controllers/data_platforms_controller.rb
+++ app/controllers/data_platforms_controller.rb
@@ -1,7 +1,9 @@
 class DataPlatformsController < ApplicationController
 
+  before_action :set_data_platform, :only => [:destroy]
+
   def index
-    @data_platforms = current_user.data_platforms
+    @data_platforms = current_user.data_platforms.includes([:platform])
   end
 
   def new
@@ -22,9 +24,20 @@ class DataPlatformsController < ApplicationController
 
   end
 
+  def destroy
+    @data_platform.destroy
+    respond_to do |format|
+      format.turbo_stream { render turbo_stream: turbo_stream.remove(@data_platform) }
+    end
+  end
+
   protected
 
   def platforms_users_params
     params.require(:data_platform).permit(:_destroy, :platform_id, :config)
+  end
+
+  def set_data_platform
+    @data_platform = current_user.data_platforms.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -20,7 +20,7 @@ Rails.application.routes.draw do
   end
 
   resources :dashboards, :only => [:index, :new, :create, :show, :destroy]
-  resources :data_platforms, :only => [:index, :new, :create]
+  resources :data_platforms, :only => [:index, :new, :create, :destroy]
   resources :connections
 
   root "file_uploads#index"
